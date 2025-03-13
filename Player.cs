4b6b92cec454db1502f6dab9d542015af54eddce
JMOWBRAY66 <jamesmowbray665@gmail.com>
2025-03-13 09:23:17 +0000

    Player.cs

--- Player.cs
+++ Player.cs
@@ -1,25 +1,27 @@
 ﻿using System.Collections.Generic;
-
-namespace DungeonExplorer
-{
-    public class Player
-    {
-        public string Name { get; private set; }
-        public int Health { get; private set; }
-        private List<string> inventory = new List<string>();
-
-        public Player(string name, int health) 
-        {
-            Name = name;
-            Health = health;
-        }
-        public void PickUpItem(string item)
-        {
-
-        }
-        public string InventoryContents()
-        {
-            return string.Join(", ", inventory);
-        }
-    }
-}
+ using System.ComponentModel;
+ 
+ namespace DungeonExplorer
+ {
+     public class Player
+     {
+         public string Name { get; private set; }
+         public int Health { get; private set; }
+         private List<string> inventory = new List<string>();
+ 
+         public Player(string name, int health) 
+         {
+             Name = name;
+             Health = health;
+         }
+ 
+         public void PickUpItem(string item)
+         {
+ 
+         }
+         public string InventoryContents()
+         {
+             return string.Join(", ", inventory);
+         }
+     }
+ }
